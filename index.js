52f2cca8948c0aefbaa84087ac9b8afdc6d1ae6f
kekojeda <kekojeda@gmail.com>
2024-10-24 10:11:23 -0300

    phonebook backend part3 ej 3.13 - 3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors')
 const app = express();
+const Person = require('./models/person')
 
 //para hacer uso de post necesitamos json-parser de express
 app.use(express.json())
@@ -16,33 +18,33 @@ morgan.token('body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
-const generateId = () => {
-    const maxId = Math.floor(Math.random() * 10000) + 1
-    return maxId
-  }
+// const generateId = () => {
+//     const maxId = Math.floor(Math.random() * 10000) + 1
+//     return maxId
+//   }
 
 const isExist = (name) => {
     const exist = 
@@ -62,19 +64,15 @@ app.get("/info", (request, response) => {
   });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id);
-  console.log(person);
-  
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(400).end();
-  }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -84,36 +82,22 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
-app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    if(!body.name ){
-        return response.status(400).json({
-            error: "name missing"
-        })
+  
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'name missing' })
     }
-    if(!body.number ){
-        return  response.status(400).json({
-            error: "number missing"
-        })
-    }
-    if(isExist(body.name)){
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-
-    persons = persons.concat(person)
-    
-    response.json(person)
-    
-})
+  
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
